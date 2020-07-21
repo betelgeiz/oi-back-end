851f78efbbc9740999ef56565610f58fd8804385
betelgeiz <k.aziret98@gmail.com>
2020-07-21 10:42:46 +0600

    Update index.js

--- app/users/index.js
+++ app/users/index.js
@@ -1,73 +1,88 @@
 'use strict'
 
 const {
-  registration: registrationSchema,
-  getuser: getUserSchema,
-  adduser: addUserSchema
+    auth: authSchema,
+    getuser: getUserSchema,
+    adduser: addUserSchema
 } = require('./schemas')
 
-module.exports = async function (fastify, opts) {
-  // Route registration
-  // fastify.<method>(<path>, <schema>, <handler>)
-  // schema is used to validate the input and serialize the output
+module.exports = async function(fastify, opts) {
+    // Route registration
+    // fastify.<method>(<path>, <schema>, <handler>)
+    // schema is used to validate the input and serialize the output
 
-  // Unlogged APIs
-  fastify.post('/register', { schema: registrationSchema }, registerHandler)
+    // Unlogged APIs
+    fastify.post('/auth', { schema: authSchema }, authHandler)
 
-  fastify.post('/add', {schema: addUserSchema}, addHandler)
+    fastify.post('/add', { schema: addUserSchema }, addHandler)
 
-  fastify.register(async function (fastify) {
+    fastify.register(async function(fastify) {
 
-    fastify.addHook('preHandler', fastify.authPreHandler)
-    fastify.get('/me', meHandler)
+        fastify.addHook('preHandler', fastify.authPreHandler)
+        fastify.get('/me', meHandler)
+        fastify.put('/', updateHandler)
 
-  })
+    })
 
-  /*fastify.setErrorHandler(function (error, request, reply) {
-    const message = error.message
-    if (errors[message]) {
-      reply.code(412)
-    }
-    reply.send(error)
-  })*/
+
+    /*fastify.setErrorHandler(function (error, request, reply) {
+      const message = error.message
+      if (errors[message]) {
+        reply.code(412)
+      }
+      reply.send(error)
+    })*/
 }
 
 // Fastify checks the existance of those decorations before registring `user.js`
 module.exports[Symbol.for('plugin-meta')] = {
-  decorators: {
-    fastify: [
-      'userService',
-      'jwt'
-    ]
-  }
+    decorators: {
+        fastify: [
+            'userService',
+            'jwt'
+        ]
+    }
 }
 
 // In all handlers `this` is the fastify instance
 // The fastify instance used for the handler registration
 
 
-async function registerHandler (req, reply) {
-  const { login, password } = req.body
-  console.log("req body =" , req.body)
-  
-  const userId = await this.userService.register(login, password)
-  return { jwt: this.jwt.sign({user: userId}) }
+async function authHandler(req, reply) {
+    const { login, password } = req.body
+    console.log("req body =", req.body)
+
+    const userId = await this.userService.auth(login, password)
+    let role
+
+    if (userId.typeuser == 1) {
+        role = 'admin'
+    } else {
+        role = 'user'
+    }
+    
+    return { jwt: this.jwt.sign({ user: userId }), role: role }
 }
 
 async function addHandler(req, reply) {
-  const {first_name, last_name} = req.body
-  console.log('body = ', req.body)
+    const { first_name, last_name } = req.body
 
-  const add = await this.userService.addUser(first_name, last_name)
-  return add
+    const add = await this.userService.addUser(first_name, last_name)
+    return add
 }
 
-async function meHandler (req, reply) {
+async function meHandler(req, reply) {
 
-  const query = await this.userService.getUser(req.user.user.id)
-  
-  return query
-  
+    const query = await this.userService.getUser(req.user.user.id)
+
+    return query
+
 }
 
+async function updateHandler(req, reply) {
+    const { login, fullname } = req.body
 
+    const query = await this.userService.update(login, fullname, req.user.user.id)
+
+    reply.code(204)
+}
